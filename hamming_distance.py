445d8d59b0394d2a4a578484ebbf67644dc9a385
tmuylder <tuurmuyldermans@gmail.com>
2019-11-06 20:36:41 +0100

    added python script

---
+++ hamming_distance.py
@@ -0,0 +1,14 @@
+# string1 and string2 should be the same length.
+def hamming_distance(string1, string2): 
+    # Start with a distance of zero, and count up
+    distance = 0
+    # Loop over the indices of the string
+    L = len(string1)
+    for i in range(L):
+        # Add 1 to the distance if these two characters are not equal
+        if string1[i] != string2[i]:
+            distance += 1
+    # Return the final count of differences
+    return distance
+
+#blabla
